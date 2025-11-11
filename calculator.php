3dcb6327cbfb0b7a5e81a63931c1e01c060c5cb9
carolina <Carolinahs100@gmail.com>
2025-11-11 16:07:31 -0700

    got the basics down.

--- calculator.php
+++ calculator.php
@@ -1,1 +1,33 @@
-// so I need to write a calculator here and then run it with php file.name
+<?php 
+function handleOperator($x, $operator, $y){
+  var_dump($operator);
+  $total = 0;
+  $x = floatval($x);
+  $y = floatval($y);
+
+  // numbers are Valid
+  switch ($operator){
+    case '+':
+      $total = $x + $y;
+      break;
+    case '-':
+       $total = $x - $y;
+      break;
+    case '/':
+       $total = $x / $y;
+      break;
+    case '*':
+       $total = $x * $y;
+      break;
+    default:
+      print "Not a valid Operator, only use *, / , + or -";
+      break;
+  }
+  print $total;
+}
+
+if ($argv[1] && $argv[2] && $argv[3]){
+handleOperator($argv[1], $argv[2], $argv[3]);
+} else {
+  print "You must provide 2 integers and an operator. Such as : 5 * 2";
+}
